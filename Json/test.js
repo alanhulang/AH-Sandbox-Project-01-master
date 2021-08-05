b8d44cf22c242bb297cebca904e3c68120f057f8
alanhulang <hl108472178@gmail.com>
2021-08-05 15:20:51 +0800

    AH

---
+++ Json/test.js
@@ -0,0 +1,19 @@
+let name = 'Alex'
+
+let person = {
+    name:'Alex',
+    bloodType:'a',
+    id:1,
+}
+let team=[
+    person={
+        name:'Alex',
+        bloodType:'a',
+        id:1,
+    },
+    person={
+        name:'Nick',
+        bloodType:'b',
+        id:2,
+    }
+]
